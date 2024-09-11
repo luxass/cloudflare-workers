c3d9ce100d6ac967c455fd4bc3a3030627327826
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-09-11 09:38:15 +0200

    test(vscode): set github_token binding

--- apps/vscode/vitest.config.ts
+++ apps/vscode/vitest.config.ts
@@ -1,5 +1,11 @@
 import { defineWorkersProject } from "@cloudflare/vitest-pool-workers/config";
 
+// eslint-disable-next-line node/prefer-global/process
+const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
+if (GITHUB_TOKEN == null) {
+  throw new Error("GITHUB_TOKEN environment variable is required");
+}
+
 export default defineWorkersProject({
   test: {
     name: "vscode",
@@ -10,6 +16,7 @@ export default defineWorkersProject({
           compatibilityFlags: ["nodejs_compat"],
           bindings: {
             ENVIRONMENT: "production",
+            GITHUB_TOKEN,
           },
         },
         wrangler: {
