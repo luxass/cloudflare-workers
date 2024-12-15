533e7f286f7020e1e66ae31605bce3afec2a5265
Lucas <lucasnrgaard@gmail.com>
2024-12-15 15:30:16 +0100

    fix: update ESLint configuration to include ignores for trusted SVG sources

--- apps/vscode/eslint.config.mjs
+++ apps/vscode/eslint.config.mjs
@@ -1,6 +1,10 @@
 // @ts-check
-import luxass from "@luxass/eslint-config";
+import { luxass } from "@luxass/eslint-config";
 
 export default luxass({
   formatters: true,
+}, {
+  ignores: [
+    "**/trusted-svg-sources.ts",
+  ],
 });
