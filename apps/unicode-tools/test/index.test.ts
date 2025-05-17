5e834d939cd75fc1442e40ef6ca1a9174a6a4249
Lucas Nørgård <lucasnrgaard@gmail.com>
2025-05-17 09:39:21 +0200

    feat: integrate @luxass/unicode-tools and enhance unicode version API (#51)

--- apps/unicode-tools/test/index.test.ts
+++ apps/unicode-tools/test/index.test.ts
@@ -7,7 +7,7 @@ import { expect, it } from "vitest";
 import worker from "../src";
 
 it("respond with a 404", async () => {
-  const request = new Request("https://luxass.dev/not-found");
+  const request = new Request("https://unicode-tools.luxass.dev/not-found");
   const ctx = createExecutionContext();
   const response = await worker.fetch(request, env, ctx);
   await waitOnExecutionContext(ctx);
@@ -22,7 +22,7 @@ it("respond with a 404", async () => {
 });
 
 it("respond with a \"pong!\"", async () => {
-  const request = new Request("https://luxass.dev/ping");
+  const request = new Request("https://unicode-tools.luxass.dev/ping");
   const ctx = createExecutionContext();
   const response = await worker.fetch(request, env, ctx);
   await waitOnExecutionContext(ctx);
