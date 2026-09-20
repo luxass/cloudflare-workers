d95c1ef4b21a10c80942e52a22bd612dcec78e59
Lucas Nørgård <lucasnrgaard@gmail.com>
2026-09-20 20:28:43 +0200

    Delete apps/notifications directory

--- apps/notifications/src/utils.ts
+++
@@ -1,9 +0,0 @@
-export async function mapConcurrent<T>(
-  items: T[],
-  concurrency: number,
-  callback: (item: T) => Promise<void>,
-) {
-  for (let index = 0; index < items.length; index += concurrency) {
-    await Promise.all(items.slice(index, index + concurrency).map(callback));
-  }
-}
